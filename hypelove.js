f7e4bd04d337af1e9ca6b5e1a8ff31b9ab3443e7
Klaus Gutenbrunner <kgutenbrunner@gmail.com>
2015-06-08 12:21:50 +0200

    add cli input support and faster loading

--- hypelove.js
+++ hypelove.js
@@ -1,79 +1,96 @@
 var casper = require('casper').create({
-    verbose: false,
-    logLevel: "error",
+    verbose: false, // false -> weniger output
+    logLevel: "debug", // error
     page: {
-    	viewportSize: { width: 1280, height: 800 }
+        viewportSize: {
+            width: 1280,
+            height: 800
+        }
     }
 });
 
 var hypeUrl = 'http://hypem.com/track/';
-var trackId = '2bqqq'; // '2bqk2'; // '2bp7c'
+var trackId = '2ammd'; // '2bqk2'; // '2bp7c'
 
+if (casper.cli.args.length == 1) {
+	trackId = casper.cli.args[0];
+}
 var loves = []
 
-function getLoves() {
-		console.log('getLoves');
 
-var l = casper.getElementsInfo('div.favcountlist ul li a span');
-    return Array.prototype.map.call(l, function(e) {
-       return e.text;
+// get information
+function getLoves() {
+    console.log('getLoves');
+    var imgElements = casper.getElementsInfo('div.favcountlist ul li a img');
+    var imgs = Array.prototype.map.call(imgElements, function(e) {
+        return e.attributes.title.replace(' loved this ', ',');
     });
+
+    return imgs;
 }
 
 
-
+// loading start page and open 'loves'
 casper.start(hypeUrl + trackId, function(selector) {
-	console.log('start');
-	this.click('#favcount_' + trackId);
-  this.wait(2000, function() {
-        this.echo("I've waited for a second.");
-    });
+    console.log('start');
+    this.click('#favcount_' + trackId);
 });
 
 
+// adding the first <= 20 entries
 casper.waitForSelector('div.favcountlist > ul > li > a', function() {
-	console.log('concat');
-
-	//this.capture('scrn.png',undefined);
-
-  loves = loves.concat(getLoves());
+    console.log('concat');
+    loves = loves.concat(getLoves());
 });
+
 
 var terminate = function() {
-    this.echo('found: ' + loves.length + '\n - ' + loves.join('\n - ')).exit();
+    this.echo('found: ' + loves.length + '\n' + loves.join('\n')).exit();
 };
 
+
+// click and extract (recursive) and terminate
 var processPage = function() {
 
-casper.then(function() {
-//	this.click('#section-track-2bp7c > div.section-player > div.act_info > div > a:nth-child(5)');
-	if (this.exists('div.act_info > div > a:nth-child(5)')) {
-		this.click('div.act_info > div > a:nth-child(5)');
-	} else {
-		this.click('div.act_info > div > a');
-	}
-	this.wait(2000, function() {
-    		if (!this.exists('div.favcountlist')) {
-    			terminate.call(casper);
-    		}
+    casper.then(function() {
+        if (this.exists('div.act_info > div > a:nth-child(5)')) { // Show more
+            this.click('div.act_info > div > a:nth-child(5)');
+        } else {
+            this.click('div.act_info > div > a'); // Next 20
+        }
+
+        // wait for loading and exit if done
+        this.waitFor(function check() {
+            var plLoading = this.evaluate(function() {
+                return $('#player-loading').css('display');
+            });
+            return plLoading === 'none';
+        }, function then() {
+            if (!this.exists('div.favcountlist')) { // div.favcountlist does not exit then exit
+                terminate.call(casper);
+            }
+
+        });
+
     });
-});
 
-casper.waitForSelector('div.favcountlist > ul > li > a', function() {
-	console.log('concat2');
 
-	var currentLoves = getLoves();
-	  console.log('concat2 ' + currentLoves + '     ' + currentLoves.count);
-	if (currentLoves.length > 0) {
-	  loves = loves.concat(currentLoves);
-	  console.log('concat2 done');
-	  return processPage.call(casper);
-	} else {
-	  console.log('concat2 exit');
-		return terminate.call(casper);
-	}
+// add new entries to list of all entries
+    casper.waitForSelector('div.favcountlist > ul > li > a', function() {
+        console.log('concat2');
 
-});
+        var currentLoves = getLoves();
+        console.log('concat2 ' + currentLoves + '     ' + currentLoves.count);
+        if (currentLoves.length > 0) {
+            loves = loves.concat(currentLoves);
+            console.log('concat2 done');
+            return processPage.call(casper);
+        } else {
+            console.log('concat2 exit');
+            return terminate.call(casper);
+        }
+
+    });
 
 };
 
@@ -81,5 +98,4 @@ casper.then(processPage);
 
 
 casper.run(function() {
-	this.echo('love = ' + loves.join('\n - ')).exit();
 });
